635747e843a2860c2faff70dd15cf98cdd8963bc
kibeomchoi <kibeom2000@gmail.com>
2022-09-29 12:52:33 +0900

    secondPC

--- python/python_6.py
+++ python/python_6.py
@@ -1,9 +1,28 @@
-data = dict()
-data['사과'] = 'apple'
-data['바나나'] = 'banana'
-data['코코넛'] = 'coconut'
+# 사전 자료형
+# data = dict()
+# data['사과'] = 'apple'
+# data['바나나'] = 'banana'
+# data['코코넛'] = 'coconut'
 
-print(data)
+# print(data)
 
-if '사과' in data :
-    print("사과 를 키로 가지는 데이터가 존재합니다.")
+# if '사과' in data :
+#     print("사과 를 키로 가지는 데이터가 존재합니다.")
+
+
+# 집합 자료형
+# a = set([1,2,3,4,5])
+# b = set([1,2,3,4,5,2])
+# print(a|b)
+# print(a&b)
+# print(a-b)
+
+a = set([1,2,3])
+a.add(4)
+print(a)
+
+a.update([5,6])
+print(a)
+a.remove(1)
+
+print(a)
---
+++ python/python_7.py
@@ -0,0 +1,6 @@
+n = int(input())
+a= list(map(int, input().split()))
+print(a)
+
+n,m,k = map(int,input().split())
+print(n,m,k)
