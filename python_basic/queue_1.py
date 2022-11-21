49623c8de5c0b3ed77aa44c53601640e38d3d249
kibeomchoi <kibeom2000@gmail.com>
2022-11-21 17:31:20 +0900

    secondPC

--- python_basic/queue_1.py
+++ python_basic/queue_1.py
@@ -6,13 +6,13 @@ queue.append(5)
 queue.append(2)
 queue.append(3)
 queue.append(7)
-queue.popleft()
+queue.popleft()         # 5 삭제
 queue.append(4)
 queue.append(1)
-queue.popleft()
+queue.popleft()         # 2 삭제
 
-print(queue)
-queue.reverse()     #역행출력
-print(queue)
+print(queue)        # 처음 들어온 원소부터 출력
+queue.reverse()     
+print(queue)        # 나중에 들어온 원소부터 출력
 
 
--- python_basic/queue_2.py
+++ python_basic/queue_2.py
@@ -5,11 +5,11 @@ queue = deque()
 queue.append(3)
 queue.append(5)
 queue.append(4)
-queue.pop()
+queue.pop()                         #입구에서 제거
 
 queue.append(1)
 queue.append(7)
-queue.popleft()
+queue.popleft()                     #출구에서 제거
 
 queue.reverse()
 print(queue)
