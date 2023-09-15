27b290856d7cdbcbe4cdd1edd91aeffdb87aa40d
kibeomchoi <kibeom2000@gmail.com>
2023-09-15 17:07:15 +0900

    commit_0915

--- this_is_cordingtest/유형별_문제/P17.py
+++ this_is_cordingtest/유형별_문제/P17.py
@@ -1,30 +1,33 @@
-import sys
+from collections import deque
 
 n,k = map(int,input().split())
-data = [list(map(int,input().split())) for _ in range(n)]
-s,x,y = map(int,input().split())
 
-dx = [0, 1, 0 ,-1]
-dy = [1 ,0 ,-1 ,0]
+graph = []
+data = []
 
-def virus(x,y,k):
-    for i in range(4):
+for i in range(n) :
+    graph.append(list(map(int,input().split())))
+    for j in range(n) :
+        if graph[i][j] != 0 :
+            data.append((graph[i][j],0,i,j))
+
+data.sort()
+q = deque(data)
+
+target_s,target_x,target_y = map(int,input().split())
+
+dx = [0,1,0,-1]
+dy = [1,0,-1,0]
+
+while q :
+    virus,s,x,y = q.popleft()
+    if s == target_s :
+        break
+    for i in range(4) :
         nx = x + dx[i]
         ny = y + dy[i]
-        if 0 <= nx < n and 0 <= ny < n and data[nx][ny] == 0:
-            data[nx][ny] = k
-
-            
-v = 1
-for _ in range(s):
-    for i in range(n):
-        for j in range(n):
-            if v > k :
-                v = 1
-            if data[i][j] == v:
-                virus(i,j,v)
-                v += 1
-                
-
-print(data)
-print(data[x-1][y-1])
+        if 0 <= nx < n and 0 <= ny < n :
+            if graph[nx][ny] == 0 :
+                graph[nx][ny] = virus
+                q.append((virus,s+1,nx,ny))
+print(graph[target_x-1][target_y-1])
