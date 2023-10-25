595380a8f52a9f2b9f90953312dcd517f54fea41
kibeomchoi <kibeom2000@gmail.com>
2023-10-25 18:28:31 +0900

    commit_1025

--- programmers_strategy/ch05/하노이의 탑.py
+++ programmers_strategy/ch05/하노이의 탑.py
@@ -1,11 +1,13 @@
-def hanoi(n,start,mid,to,answer) :
+def hanoi(n,start,mid,end,answer) :
   if n == 1 :
-    return answer.append([start,to])
-  hanoi(n-1,start,mid,to,answer)
-  answer.append([start.to])
-  hanoi(n-1,mid,to,start,answer)
+    return answer.append((start,mid))
+  else :
+    hanoi(n-1,start,end,mid,answer)
+    hanoi(1,start,mid,end,answer)
+    hanoi(n-1,mid,start,end,answer)
 
-def solution(n) :
-  answer = []
-  hanoi(n,1,3,2,answer)
-  return answer
+answer = []
+n = int(input())
+hanoi(n,1,2,3,answer)
+for i in answer :
+  print(*i)
