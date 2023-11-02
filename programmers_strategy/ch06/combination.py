6e40bec78e9a74dc5d0328b6b01f4f7fa2b63fac
kibeomchoi <kibeom2000@gmail.com>
2023-11-02 19:07:44 +0900

    commit_1102

--- programmers_strategy/ch06/combination.py
+++ programmers_strategy/ch06/combination.py
@@ -2,20 +2,19 @@ n,m = map(int,input().split())
 arr = sorted(map(int,input().split()))
 visited = [False] * n
 result = []
+
 def dfs() :
     if len(result) == m :
         print(*result)
-        return 
+        return
     else :
         remember = ''
         for i in range(n) :
-            if visited[i] == False and remember != arr[i] :
-                visited[i] = True
+            if not visited[i] and remember != arr[i] :
                 result.append(arr[i])
+                visited[i] = True
                 remember = arr[i]
                 dfs()
                 result.pop()
                 visited[i] = False
-                
-
 dfs()
