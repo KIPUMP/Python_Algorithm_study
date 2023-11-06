3fd103a2a2490c7cf276305b8efe98708aaba5ac
kibeomchoi <kibeom2000@gmail.com>
2023-11-06 17:49:11 +0900

    commit_1106

---
+++ programmers_strategy/ch07/bubble_sort.py
@@ -0,0 +1,10 @@
+sample = [3,0,1,8,7,2,5,4,6,9]
+
+def bubble_sort(data) :
+  for i in range(len(data) - 1) :
+    for j in range(i) :
+      if data[i] > data[j+1] :
+        data[j],data[j+1] = data[j+1],data[j]
+
+bubble_sort(sample)
+print(sample)
