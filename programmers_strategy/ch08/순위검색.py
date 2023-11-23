5b1a3396ac3bcf1bb999726ebbbb4886c6b016dd
kibeomchoi <kibeom2000@gmail.com>
2023-11-23 19:18:26 +0900

    commit_1123

---
+++ programmers_strategy/ch08/순위검색.py
@@ -0,0 +1,91 @@
+from collections import defaultdict
+from itertools import combinations
+from bisect import bisect_left,bisect_right
+
+def solution(info,query) :
+  answer = []
+  people = defaultdict(list)
+
+  for i in info :
+    person = i.split()
+    score = int(person.pop())
+    people = [''.join(person)].append(score)
+
+    for j in range(4) :
+      candi = list(combinations(person,j))
+      for c in candi :
+        people[''.join(c)].append(score)
+    
+    for i in people :
+      people[i].sort()
+
+    for i in query :
+      key = i.split()
+      score = int(key.pop())
+      key = ''.join(key)
+      key = key.replace('and','').replace(' ','').replace('-','')
+      answer.append(len(people[key]) - left_bound(people[key],score))
+  return answer
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
